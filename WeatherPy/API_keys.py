d8511c83b805beb080a1410374599e4c359e60ff
Pamela Hernandez <pamelahernandez@Pamelas-MacBook-Pro.local>
2020-09-07 17:40:31 -0700

    HW DONE

--- WeatherPy/API_keys.py
+++ WeatherPy/API_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "..."
+weather_api_key = "9134281e5a366842cdf139a147d00079"
 
 # Google API Key
-g_key = "..."
+g_key = "AIzaSyArYwnxTcb2xZko85VPL0yMOJ0BrW1KGe4"
